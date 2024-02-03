7a4ecb552553291eca8e469c0ca1b50c4e1eea5b
rkvrpl <rkvrpl@icloud.com>
2024-02-03 23:52:11 +0300

    have problem with number

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,33 +5,49 @@ const choice = document.querySelector('.choice'),
       result = document.querySelector('.result'),
       error = document.querySelector('.error');
 
+      console.log(typeof Number(number.value));
     function check() {
     try {
-        if(choice.value === `select`){
+        if(choice.value == `select`){
             throw new Error(`Выберите предложенную категорию`);
         }
-    else if(number.value > 10 || number.value === ''){
-        throw new Error(`Вы ввели ${number.value}. Необходимо ввести число от 1 до 10`);
+        else if(Number(number.value) > 10 || Number(number.value) === ' '){
+            throw new Error(`Вы ввели ${number.value}.Для категории люди и планеты еобходимо ввести число от 1 до 10 и для категории фильмы от 1 до 6.`);
+        }
+        result.textContent = '';
+        error.textContent = '';
+    } catch (e) {
+        error.textContent = `${e.message}`;
     }
-    result.textContent = '';
-    error.textContent = '';
-    } catch (error) {
-        error.textContent = `${error.message}`;
     }
+
+    function checkFilm(){
+        try {
+            if(number.value >= 7 ){
+                throw new Error(`Вы ввели ${number.value}. Для категории фильмы необходимо ввести число от 1 до 6.`);
+            }
+            result.textContent = '';
+            error.textContent = '';
+        } catch (e) {
+            error.textContent = `${e.message}`;
+        }
     }
+
     async function getChoice() {
         try{
             const response = await fetch(`https://swapi.dev/api/${choice.value}/${number.value}/`);
             const data = await response.json();
             if (data.name === undefined) {
-                result.textContent = `Результат поиска: ${data.title}`;
+                result.textContent = `Результат: ${data.title}`;
             } else {
-                result.textContent = `Результат поиска: ${data.name}`;
+                result.textContent = `Результат: ${data.name}`;
             }
-        }catch(error){
+            error.textContent = '';
+        }catch(e){
             check();
+            checkFilm();
         }finally {
-        console.log('Готово');
+            console.log('Готово');
         }
     }
     button.addEventListener('click', getChoice);
