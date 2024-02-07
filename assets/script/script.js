660e299d74e1524cd420d69957fb46e0f99a78e7
rkvrpl <rkvrpl@icloud.com>
2024-02-07 16:02:29 +0300

    change js

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,8 +10,9 @@ const choice = document.querySelector('.choice'),
         if(choice.value == `select`){
             throw new Error(`Выберите предложенную категорию`);
         }
-        else if(number.value > 10 || number.value === ' ' || (choice.value === 'films' && number.value >= 7)){
-            throw new Error(`Вы ввели ${number.value}. Для категории люди и планеты необходимо ввести число от 1 до 10 и для категории фильмы от 1 до 6.`);
+        else if(number.value > 10 || number.value === '' || (choice.value === 'films' && number.value >= 7)){
+            console.log("сложное условие");
+            throw new Error(`Вы ввели "${number.value}". Для категории люди и планеты необходимо ввести число от 1 до 10 и для категории фильмы от 1 до 6.`);
         }
         result.textContent = '';
         error.textContent = '';
@@ -22,19 +23,24 @@ const choice = document.querySelector('.choice'),
 
 
     async function getChoice() {
+        const response = await fetch(`https://swapi.dev/api/${choice.value}/${number.value}/`);
+        const data = await response.json();
+        console.log(data);
+        console.log(data.name);
         try{
-            const response = await fetch(`https://swapi.dev/api/${choice.value}/${number.value}/`);
-            const data = await response.json();
-            if (data.name === undefined) {
-                result.textContent = `Результат: ${data.title}`;
+            check();
+            if (data.results) {
+                console.log(data.results);
+                return;
+            } else if (data.name === undefined) {
+                result.textContent = `Фильм: ${data.title}`;
             } else {
-                result.textContent = `Результат: ${data.name}`;
+                result.textContent = `Имя: ${data.name}`;
             }
             error.textContent = '';
         }catch(e){
+            console.log("Ошибка", e);
             check();
-        }finally {
-            console.log('Готово');
         }
     }
-    button.addEventListener('click', getChoice);
+    button.addEventListener('click', getChoice); 
